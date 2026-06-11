515e4a670e4a6ad97de5ed568f0130a0b45b3bbd
qtung52 <quangtung050206@gmail.com>
2026-06-11 17:08:13 +0700

    Configure base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Thêm dòng này để chạy được trên GitHub Pages
 })
